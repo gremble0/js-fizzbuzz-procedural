f47a55b7c332f91693b027c1ee10ca93d591b474
gremble0 <hermanstornes@gmail.com>
2024-09-23 11:10:05 +0200

    Complete exercise

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,26 @@
-const answer = []
+/**
+ * @param {number} num
+ * @returns {any[]}
+ */
+function fizzbuzz(num) {
+  let out = new Array(num)
 
-// Write your code below this line
+  let iter = ''
+  for (let i = 1; i <= num; ++i) {
+    if (i % 3 == 0) iter += 'Fizz'
+    if (i % 5 == 0) iter += 'Buzz'
 
+    if (iter === '') out[i - 1] = i
+    else {
+      out[i - 1] = iter
+      iter = ''
+    }
+  }
 
+  return out
+}
 
-
+const answer = fizzbuzz(15)
 
 // Don't touch the code below this line, we'll cover it later
 module.exports = answer
